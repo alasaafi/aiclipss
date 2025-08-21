b60abab9e5d1585af9a563b4f2b92c2a2b402b2e
Alasaafi <alasaafi34@gmail.com.com>
2025-08-21 14:59:23 +0100

    Update wsgi.py with new changes

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ from waitress import serve
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render يعطي PORT تلقائيًا
-    serve(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host="0.0.0.0", port=port)  # ✅ Correct
