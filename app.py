02cae222c007d0bfcad16b6c70134136061223fb
Alasaafi <alasaafi34@gmail.com.com>
2025-08-21 14:59:57 +0100

    Update app.py with new changes

--- app.py
+++ app.py
@@ -588,5 +588,6 @@ def perform_upload():
 
 
 if __name__ == '__main__':
-    pass
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port)
     
