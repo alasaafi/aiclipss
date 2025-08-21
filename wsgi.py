bff40cbfa7f3d3b38cdcdb5e6ec2380170d7aef6
Alasaafi <alasaafi34@gmail.com.com>
2025-08-21 15:27:58 +0100

    Update wsgi.py with new changes

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-import os
-from waitress import serve
 from app import app
 
 if __name__ == "__main__":
+    # This is just for local development
+    import os
     port = int(os.environ.get("PORT", 5000))
-    serve(app, host="0.0.0.0", port=port)  # ✅ Correct
+    app.run(host="0.0.0.0", port=port)
