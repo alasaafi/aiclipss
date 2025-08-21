c3ab6aae1beb10c19fba98369324cf71ebb8ab8d
Alasaafi <alasaafi34@gmail.com.com>
2025-08-21 10:15:55 +0100

    Update wsgi.py with new changes

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,2 @@
-import os # Import the 'os' module
-from waitress import serve
 from app import app
 
-if __name__ == '__main__':
-    # Get port from environment variable, or default to 10000 for local use
-    port = int(os.environ.get("PORT", 10000))
-    print(f"Starting server on http://0.0.0.0:{port}")
-    serve(app, host='0.0.0.0', port=port) 
